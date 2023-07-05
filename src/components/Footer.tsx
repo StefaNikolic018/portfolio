de8b94bf2ce555961fb19947b52fb1f7f1ebc6fb
SweetJonesUGK <stefanikolic018@gmail.com>
2023-07-05 14:30:57 +0200

    style: :lipstick: finished footer v1.0

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,26 @@
 import React, { memo } from 'react'
+import { TfiWorld } from 'react-icons/tfi'
+
+import logo from './../assets/sn-logo.svg'
 
 function Footer() {
   return (
-    <footer className="bg-gray-100 py-10">
-      <div className="container mx-auto">
-        <p className="text-center text-gray-500">
-          © {new Date().getFullYear()} {import.meta.env.VITE_USERNAME}
-        </p>
+    <footer className="border-t-2 border-t-black bg-gray-100 py-10">
+      <div className="container flex flex-row items-center justify-around">
+        <div className="flex w-4/12 justify-center">
+          <img alt="Logo" src={logo} className="h-12 w-12" />
+        </div>
+        <span className="w-4/12 text-center uppercase text-gray-500">
+          BY {import.meta.env.VITE_USERNAME}
+        </span>
+        <span className="flex w-4/12 flex-row items-center justify-center gap-2">
+          <TfiWorld className="text-4xl" />
+          <div className="">
+            © {new Date().getFullYear()} SN
+            <br />
+            ALL RIGHTS RESERVED.
+          </div>
+        </span>
       </div>
     </footer>
   )
