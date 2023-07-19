67094347d3cb94f0f4fe6344bfb0191c1f581750
Stefan Nikolic <stefanikolic018@gmail.com>
2023-07-19 19:24:42 +0200

    style: :bug: fixed navigation z index

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ import Navigation from './Navigation'
 
 function Header() {
   return (
-    <header className="sticky top-0 z-10 bg-white shadow-sm">
+    <header className="sticky top-0 z-20 bg-white shadow-sm">
       <div className="container mx-auto flex items-center justify-between py-4 px-8">
         <img alt="Logo" src={logo} className="h-10 w-10" />
         <Navigation />
