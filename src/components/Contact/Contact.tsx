570bb24e8f3ecd133531a5df8683dee8c1cd738b
SweetJonesUGK <stefanikolic018@gmail.com>
2023-08-04 14:20:35 +0200

    refactor: :recycle: added more social media contacts

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -9,7 +9,7 @@ const Contact = () => {
   return (
     <section className="relative h-[50vh]">
       <div className="absolute top-0 left-[15%] z-10 flex h-full w-1/3 flex-col items-center justify-center bg-gray-900 bg-opacity-20 bg-clip-padding px-14 text-white backdrop-blur-sm backdrop-filter">
-        <h2 className="mt-10 mb-3 font-dalfitra text-5xl font-bold tracking-wider">
+        <h2 className="mt-5 mb-3 font-dalfitra text-5xl font-bold tracking-wider">
           CONTACTS:
         </h2>
         <Info />
--- src/components/Contact/Social.tsx
+++ src/components/Contact/Social.tsx
@@ -1,6 +1,7 @@
 import React, { memo } from 'react'
 
 import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
+import { FaViber, FaTelegram, FaWhatsapp } from 'react-icons/fa'
 
 type contactItem = {
   icon: React.ReactElement
@@ -11,29 +12,50 @@ type contactItem = {
 const socialItems: contactItem[] = [
   {
     icon: (
-      <AiFillGithub className="mr-4 inline-block text-4xl transition-all duration-150 group-hover:scale-125" />
+      <AiFillGithub className="mr-4 inline-block text-4xl transition-all duration-150 group-hover:scale-110 group-hover:text-black" />
     ),
     link: 'https://github.com/StefaNikolic018',
     name: 'GITHUB'
   },
   {
     icon: (
-      <AiFillLinkedin className="mr-4 inline-block text-4xl transition-all duration-150 group-hover:scale-125" />
+      <AiFillLinkedin className="mr-4 inline-block text-4xl transition-all duration-150 group-hover:scale-110 group-hover:text-blue-400" />
     ),
     link: 'https://www.linkedin.com/in/stefanikolic18/',
     name: 'LINKEDIN'
+  },
+  {
+    icon: (
+      <FaViber className="mr-4 inline-block text-4xl transition-all duration-150 group-hover:scale-110 group-hover:text-purple-400" />
+    ),
+    link: 'viber://contact?number=%2B381611555036',
+    name: 'VIBER'
+  },
+  {
+    icon: (
+      <FaTelegram className="mr-4 inline-block text-4xl transition-all duration-150 group-hover:scale-110 group-hover:text-sky-400" />
+    ),
+    link: 'https://t.me/telefonov1',
+    name: 'TELEGRAM'
+  },
+  {
+    icon: (
+      <FaWhatsapp className="mr-4 inline-block text-4xl transition-all duration-150 group-hover:scale-110 group-hover:text-green-400" />
+    ),
+    link: 'https://wa.me/381611555036?text=Hi20%Stefan20%can20%we20%talk20%about20%your20%web20%development20%services',
+    name: 'WHATSAPP'
   }
 ]
 
 const social = socialItems.map((item: contactItem) => (
   <div
     key={item.name}
-    className="group flex w-full flex-row items-center justify-center rounded-md bg-gray-500 bg-opacity-10 bg-clip-padding p-5 backdrop-blur-sm backdrop-filter hover:bg-opacity-5"
+    className="group flex w-full flex-row items-center justify-center rounded-md bg-gray-900 bg-opacity-10 bg-clip-padding p-5 backdrop-blur-sm backdrop-filter hover:bg-opacity-5"
   >
     <a
       key={item.name}
       href={item.link}
-      className="w-full text-lg transition-all duration-150"
+      className="w-full text-sm text-gray-200 transition-all duration-150 group-hover:text-white"
       target="_blank"
     >
       {item.icon} {item.name}
@@ -43,7 +65,7 @@ const social = socialItems.map((item: contactItem) => (
 
 const Social = () => {
   return (
-    <div className="mt-5 flex w-full flex-col justify-start gap-5">
+    <div className="mt-5 flex w-full flex-col justify-start gap-2">
       {social}
     </div>
   )
